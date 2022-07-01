d7865c73b40205e1fd8182e6e381c4d0c87f77a3
albiancdp <albiancdp@gmail.com>
2022-07-02 00:13:55 +0700

    add: handle if thumbnail book null

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -1,8 +1,8 @@
 const apiCors = {
   origin: ['*'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition', 'allow-control-allow-origin'],
-  exposedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition', 'allow-control-allow-origin'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition', 'Allow-Control-Allow-Origin'],
+  exposedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition', 'Allow-Control-Allow-Origin'],
   maxAge: 3600,
   credentials: true,
   preflightContinue: false,
