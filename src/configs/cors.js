9684c8f63251bce42fcfaef77d201bea5e7ba6f5
albiancdp <albiancdp@gmail.com>
2022-07-01 22:52:18 +0700

    change: npm run start

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -1,8 +1,8 @@
 const apiCors = {
   origin: ['*'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition'],
-  exposedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition', 'allow-control-allow-origin'],
+  exposedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Content-Disposition', 'allow-control-allow-origin'],
   maxAge: 3600,
   credentials: true,
   preflightContinue: false,
